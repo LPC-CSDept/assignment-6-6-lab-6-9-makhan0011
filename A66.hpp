17f3ecb6763ad4dd3f50304adb8d96363a8752af
makhan0011 <112144798+makhan0011@users.noreply.github.com>
2022-11-10 21:12:20 +0000

    c

--- A66.hpp
+++ A66.hpp
@@ -1,7 +1,32 @@
-//
-
-#include <iomanip>
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-// complete the function maxmin( )
+int getInput(int &, int &, int &);
+int maxMin(int, int, int, int &, int &);
+
+int getInput(int &n1, int &n2, int &n3)
+{
+    cin >> n1;
+    cin >> n2;
+    cin >> n3;
+}
+
+int maxMin(int n1, int n2, int n3, int &max, int &min)
+{
+    if ((n1 < n2 && n1 < n3))
+        min = n1;
+    else if ((n2 < n1 && n2 < n3))
+        min = n2;
+    else if ((n3 < n1 && n3 < n2))
+        min = n3;
+
+    if ((n1 > n2 && n1 > n3))
+        max = n1;
+    else if ((n2 > n1 && n2 > n3))
+        max = n2;
+    else if ((n3 > n2 && n3 > n1))
+        max = n3;
+
+    return min, max;
+}
